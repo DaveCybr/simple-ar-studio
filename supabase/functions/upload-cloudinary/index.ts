47b7a17a61037a2352c59b9d5fdde6a77ff6f6b9
DaveCybr <davidrngg4@gmail.com>
2025-12-24 18:18:29 +0700

    push

--- supabase/functions/upload-cloudinary/index.ts
+++ supabase/functions/upload-cloudinary/index.ts
@@ -74,7 +74,7 @@ serve(async (req) => {
 
     if (resourceType === "video") {
       transformation =
-        "q_auto:good,w_640,h_360,c_limit,vc_h264:baseline:3.0,ac_aac,br_1500k";
+        "q_auto:good,w_1920,h_1080,c_limit,vc_h264:main:4.0,ac_aac,br_4000k";
     } else if (resourceType === "image" && folder === "ar-content") {
       transformation = "q_auto:good,w_1024,h_1024,c_limit,f_auto";
     } else if (resourceType === "image" && folder === "ar-markers") {
