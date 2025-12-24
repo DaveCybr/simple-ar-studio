946ad82dde290a2c7b760b91f231651a4abbd1aa
DaveCybr <davidrngg4@gmail.com>
2025-12-24 16:40:46 +0700

    push

--- src/pages/ViewAR.tsx
+++ src/pages/ViewAR.tsx
@@ -156,8 +156,7 @@ const ViewAR = () => {
         }))
       )
     );
-    // viewerUrl = `/arjs-viewer.html?markers=${markersParam}`;
-    viewerUrl = `/ar-debug.html?markers=${markersParam}`;
+    viewerUrl = `/arjs-viewer.html?markers=${markersParam}`;
   }
 
   return (
